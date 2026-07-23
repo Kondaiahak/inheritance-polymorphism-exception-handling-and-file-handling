9198d6c8c603d05822343b021b47f3782659bb47
P kondaiah <kondaiah3006@gmail.com>
2026-07-23 11:21:13 +0530

    Add files via upload

---
+++ inheritence/hour_permanent/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+interface IPayable
+{
+    void CalculatePay();
+}
+
+class Person
+{
+    protected string firstName;
+    protected string lastName;
+
+    public Person(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void DisplayPerson()
+    {
+        Console.WriteLine("Name : " + firstName + " " + lastName);
+    }
+}
+
+class HourlyEmployee : Person, IPayable
+{
+    public double HoursWorked;
+    public double PayPerHour;
+    public double TotalPay;
+
+    public HourlyEmployee(string fname, string lname, double hours, double pay)
+        : base(fname, lname)
+    {
+        HoursWorked = hours;
+        PayPerHour = pay;
+    }
+
+    public void CalculatePay()
+    {
+        TotalPay = HoursWorked * PayPerHour;
+    }
+
+    public void Display()
+    {
+        DisplayPerson();
+        Console.WriteLine("Hours Worked : " + HoursWorked);
+        Console.WriteLine("Pay Per Hour : " + PayPerHour);
+        Console.WriteLine("Total Pay    : " + TotalPay);
+    }
+}
+
+class PermanentEmployee : Person, IPayable
+{
+    public double BasicSalary;
+    public double HRA;
+    public double DA;
+    public double Tax;
+    public double NetPay;
+    public double TotalPay;
+
+    public PermanentEmployee(string fname, string lname, double salary)
+        : base(fname, lname)
+    {
+        BasicSalary = salary;
+    }
+
+    public void CalculatePay()
+    {
+        HRA = BasicSalary * 0.15;
+        DA = BasicSalary * 0.10;
+        TotalPay = BasicSalary + HRA + DA;
+        Tax = TotalPay * 0.08;
+        NetPay = TotalPay - Tax;
+    }
+
+    public void Display()
+    {
+        DisplayPerson();
+        Console.WriteLine("Basic Salary : " + BasicSalary);
+        Console.WriteLine("HRA          : " + HRA);
+        Console.WriteLine("DA           : " + DA);
+        Console.WriteLine("Total Pay    : " + TotalPay);
+        Console.WriteLine("Tax          : " + Tax);
+        Console.WriteLine("Net Pay      : " + NetPay);
+    }
+}
+
+class Program
+{
+    static string ReadRequiredText(string prompt)
+    {
+        while (true)
+        {
+            Console.Write(prompt);
+            string? input = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+
+            Console.WriteLine("Please enter a value.");
+        }
+    }
+
+    static double ReadDouble(string prompt)
+    {
+        while (true)
+        {
+            Console.Write(prompt);
+            string? input = Console.ReadLine();
+
+            if (double.TryParse(input, out double value))
+            {
+                return value;
+            }
+
+            Console.WriteLine("Please enter a valid number.");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hourly Employee");
+
+        string hfname = ReadRequiredText("First Name: ");
+        string hlname = ReadRequiredText("Last Name: ");
+        double hours = ReadDouble("Hours Worked: ");
+        double pay = ReadDouble("Pay Per Hour: ");
+
+        HourlyEmployee h = new HourlyEmployee(hfname, hlname, hours, pay);
+
+        h.CalculatePay();
+
+        Console.WriteLine("\nHourly Employee Details");
+        h.Display();
+
+        Console.WriteLine("\nPermanent Employee");
+
+        string pfname = ReadRequiredText("First Name: ");
+        string plname = ReadRequiredText("Last Name: ");
+        double salary = ReadDouble("Basic Salary: ");
+
+        PermanentEmployee p = new PermanentEmployee(pfname, plname, salary);
+
+        p.CalculatePay();
+
+        Console.WriteLine("\nPermanent Employee Details");
+        p.Display();
+    }
+}
---
+++ inheritence/person/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Globalization;
+
+class Person
+{
+    private string firstName;
+    private string lastName;
+    private string emailAddress;
+    private DateTime dateOfBirth;
+
+    public Person(string firstName, string lastName, string emailAddress, DateTime dateOfBirth)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public bool IsAdult
+    {
+        get
+        {
+            int age = DateTime.Now.Year - dateOfBirth.Year;
+
+            if (DateTime.Now < dateOfBirth.AddYears(age))
+                age--;
+
+            return age >= 18;
+        }
+    }
+
+    public string SunSign
+    {
+        get
+        {
+            int day = dateOfBirth.Day;
+            int month = dateOfBirth.Month;
+
+            if ((month == 3 && day >= 21) || (month == 4 && day <= 19))
+                return "Aries";
+            else if ((month == 4 && day >= 20) || (month == 5 && day <= 20))
+                return "Taurus";
+            else if ((month == 5 && day >= 21) || (month == 6 && day <= 20))
+                return "Gemini";
+            else if ((month == 6 && day >= 21) || (month == 7 && day <= 22))
+                return "Cancer";
+            else if ((month == 7 && day >= 23) || (month == 8 && day <= 22))
+                return "Leo";
+            else if ((month == 8 && day >= 23) || (month == 9 && day <= 22))
+                return "Virgo";
+            else if ((month == 9 && day >= 23) || (month == 10 && day <= 22))
+                return "Libra";
+            else if ((month == 10 && day >= 23) || (month == 11 && day <= 21))
+                return "Scorpio";
+            else if ((month == 11 && day >= 22) || (month == 12 && day <= 21))
+                return "Sagittarius";
+            else if ((month == 12 && day >= 22) || (month == 1 && day <= 19))
+                return "Capricorn";
+            else if ((month == 1 && day >= 20) || (month == 2 && day <= 18))
+                return "Aquarius";
+            else
+                return "Pisces";
+        }
+    }
+
+    public bool IsBirthday
+    {
+        get
+        {
+            return DateTime.Now.Day == dateOfBirth.Day &&
+                   DateTime.Now.Month == dateOfBirth.Month;
+        }
+    }
+
+    public string ScreenName
+    {
+        get
+        {
+            return firstName.ToLower() +
+                   lastName.ToLower() +
+                   dateOfBirth.Day.ToString("00") +
+                   dateOfBirth.Month.ToString("00");
+        }
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("\nPerson Details");
+        Console.WriteLine("------------------------");
+        Console.WriteLine("First Name   : " + firstName);
+        Console.WriteLine("Last Name    : " + lastName);
+        Console.WriteLine("Email        : " + emailAddress);
+        Console.WriteLine("Date of Birth: " + dateOfBirth.ToString("dd/MM/yyyy"));
+        Console.WriteLine("Is Adult     : " + IsAdult);
+        Console.WriteLine("Sun Sign     : " + SunSign);
+        Console.WriteLine("Is Birthday  : " + IsBirthday);
+        Console.WriteLine("Screen Name  : " + ScreenName);
+    }
+}
+
+class Employee : Person
+{
+    public double Salary { get; set; }
+
+    public Employee(string firstName, string lastName, string email,
+        DateTime dob, double salary)
+        : base(firstName, lastName, email, dob)
+    {
+        Salary = salary;
+    }
+
+    public void DisplayEmployee()
+    {
+        Display();
+        Console.WriteLine("Salary       : " + Salary);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter First Name: ");
+        string fname = Console.ReadLine();
+
+        Console.Write("Enter Last Name: ");
+        string lname = Console.ReadLine();
+
+        Console.Write("Enter Email: ");
+        string email = Console.ReadLine();
+
+        Console.Write("Enter Date of Birth (dd/MM/yyyy): ");
+        DateTime dob = DateTime.ParseExact(
+            Console.ReadLine(),
+            "dd/MM/yyyy",
+            CultureInfo.InvariantCulture);
+
+        Console.Write("Enter Salary: ");
+        double salary = Convert.ToDouble(Console.ReadLine());
+
+        Employee emp = new Employee(fname, lname, email, dob, salary);
+
+        emp.DisplayEmployee();
+    }
+}
