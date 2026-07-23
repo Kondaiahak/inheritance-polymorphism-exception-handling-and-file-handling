08bd0269ff7fc0127987ee82c746840661f552dd
P kondaiah <kondaiah3006@gmail.com>
2026-07-23 11:20:06 +0530

    Add files via upload

---
+++ file handling/console_application/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter file name: ");
+        string fileName = Console.ReadLine();
+
+        Console.Write("Enter the content to write into the file: ");
+        string content = Console.ReadLine();
+
+        StreamWriter writer = new StreamWriter(fileName + ".txt");
+
+        writer.WriteLine(content);
+
+        writer.Close();
+
+        Console.WriteLine("\nFile created successfully.");
+        Console.WriteLine("Content written and file closed.");
+    }
+}
